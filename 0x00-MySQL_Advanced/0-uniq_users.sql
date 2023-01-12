a22e26dd438c7b9b11d7f19e290c54f6a96cbe69
ayobamy <ayobamyahmed@gmail.com>
2023-01-12 22:28:09 +0000

    [feat]: create table

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- a SQL script that creates a table users
+CREATE TABLE IF NOT EXISTS users;
+INSERT INTO users (
+	id INT NOT NULL AUTO INCREMENT PRIMARY KEY
+	email VARCHAR(255) NOT NULL UNIQUE
+	name VARCHAR(255)
+);
