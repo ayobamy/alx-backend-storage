8c708aa0b0ef61c69e3ee61c9616692d1399bb4d
ayobamy <ayobamyahmed@gmail.com>
2023-01-18 23:37:56 +0000

    [update]: updated redis class with random key generator

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,8 +3,30 @@
 Writing strings to Redis
 """
 import redis
+import uuid
+from typing import Any
 
 
 class Cache:
-    pass
+    """
+    redis cache
+    """
+    def __init__(self):
+        """
+        store an instance of the
+        redis client
+        """
+        _redis = redis.Redis()
+        _redis.flushdb()
 
+    def store(self, data: Any) -> str:
+        """
+        a method that takes a data
+        argument and returns a string
+        """
+        r = redis.Redis()
+        key = str(uuid.uuid4())
+
+        r.set(key, data)
+
+        return key
