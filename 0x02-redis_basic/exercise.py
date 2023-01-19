0923beb1ebf7b018b73373807b9f06e7c23cf7d8
ayobamy <ayobamyahmed@gmail.com>
2023-01-19 00:58:02 +0000

    [update]: updated with get method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Writing strings to Redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -29,3 +29,35 @@ class Cache:
         self._redis.set(key, data)
 
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> \
+            Union[str, bytes, int, float]:
+        """
+        a get method that take a key
+        string argument and an optional
+        Callable argument named fn.
+        """
+        value = self._redis.get(key)
+
+        if value is not None:
+            if fn:
+                value = fn(value)
+            return value
+        else:
+            return None
+
+    def get_str(self, val: bytes) -> str:
+        """
+        automatically parametrize Cache.get
+        returns a string
+        """
+        return str(val, val.decode('utf-8'))
+
+    def get_int(self, val: bytes) -> int:
+        """
+        automatically parametrize Cache.get
+        returns an int
+        """
+        val = int(val, val.decode('utf-8'))
+
+        return val if val else 0
