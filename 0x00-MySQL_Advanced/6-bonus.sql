1e316ccf64c0b1f56b1aeddf7f5f689941b9e914
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2023-02-27 03:59:15 +0000

    [feat]: recreates a stored procedure AddBonus

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,4 +1,4 @@
--- SQL script that creates a stored procedure AddBonus that adds a new correction for a student
+-- script that creates a stored procedure AddBonus that adds a new correction for a student
 -- Procedure AddBonus is taking 3 inputs (in this order):
 -- user_id, a users.id value (you can assume user_id is linked to an existing users)
 -- project_name, a new or already exists projects - if no projects.name found in the table, you should create it
